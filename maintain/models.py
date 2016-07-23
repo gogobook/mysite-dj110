48ca5d9abf0f3132c1b8df79816fedd9e927a7b2
gogobook <O968041428@gmail.com>
2016-07-23 21:37:57 +0800

    Auto stash for revert of "more models"

--- maintain/models.py
+++ maintain/models.py
@@ -15,6 +15,32 @@ class Part(models.Model):
     car = models.ForeignKey(car）
     category = models.CharField(max_length=200)
     supplier = models.CharField(max_length=200)
+<<<<<<< Updated upstream
+=======
+    price = models.IntegerField(default=0)
+
+class Work(models.Model):
+    name = models.CharField(max_length=200)
+    price = models.IntegerField(default=0)
+
+class Consumer(models.Model):
+    name = models.CharField(max_length=200)
+    phoneNo = models.IntegerField(default=0)
+    car = models.ForeignKey(car）
+    brandNo = models.CharField(max_length=200)
+
+class Master(models.Model):
+    name = models.CharField(max_length=200)
+
+class WorkOrder(models.Model):
+    date = models.DateField()
+    consumer = models.ForeignKey()
+    master = models.ForeignKey()
+    part = models.ForeignKey()
+    work = models.ForeignKey()
+    totalprice = models.IntegerField(default=0)
+
+>>>>>>> Stashed changes
 
 
 
